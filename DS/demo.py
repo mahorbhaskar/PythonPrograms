0e1c71a28cc088c72948546a08d32d2583d392c5
Shiva <bhaskarmahor1@gmail.com>
2021-07-14 01:30:50 +0530

    write from 5th line line

--- DS/demo.py
+++ DS/demo.py
@@ -1,1 +1,9 @@
 print("Hello World")
+# lets check a little program
+# enter the all even nubmer between 1 to 100
+
+for x in range(1,100):
+    if (x%2==0):
+        print(x)
+    else:
+        pass
