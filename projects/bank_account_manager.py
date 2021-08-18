eb259b521fc2081e65e296562d61078d13ee8174
mahorbhaskar <bhaskarmahor1@Gmail.com>
2021-08-18 10:04:31 +0530

    first commit

---
+++ projects/bank_account_manager.py
@@ -0,0 +1,6 @@
+'''Bank Account Manager
+Under the Classes section in the list of suggested final capstone projects is a Bank Account Manager program.
+The goal is to create a class called Account which will be an abstract class for three other classes called
+CheckingAccount, SavingsAccount and BusinessAccount.
+Then you should manage credits and debits from these accounts through an ATM style program.'''
+
