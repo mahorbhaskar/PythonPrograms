b7c37a54cfb433c9152a78f734ba4b36a2db45e9
Bhaskar Mahor <bhaskarmahor1@gmail.com>
2021-09-06 11:53:33 +0530

    updated

--- projects/guessingGame.py
+++ projects/guessingGame.py
@@ -17,3 +17,45 @@ When the player's guess equals the number, tell them they've guessed correctly a
 First, pick a random integer from 1 to 100 using the random module and assign it to a variable
 Note: random.randint(a,b) returns a random integer in range [a, b], including both end points.
 '''
+import random
+
+num = random.randint(1,100)
+
+print("WELCOME TO GUESS ME!")
+print("I'm thinking of a number between 1 and 100")
+print("If your guess is more than 10 away from my number, I'll tell you you're COLD")
+print("If your guess is within 10 of my number, I'll tell you you're WARM")
+print("If your guess is farther than your most recent guess, I'll say you're getting COLDER")
+print("If your guess is closer than your most recent guess, I'll say you're getting WARMER")
+print("LET'S PLAY!")
+
+guesses = [0]
+
+while True:
+    guess = int(input("I'm thinking of a Number between 1 to 100.\nWhat is your Guess?"))
+
+    if guess < 1 or guess > 100:
+        print('Out of Bounds! Please Try Again..')
+        continue
+    # here we compare the guess to the number
+    if guess == num:
+        print(f'Congratulations,The number was {num}\nYou guessed it in only {len(guesses)} Guesses! ')
+        break
+
+    # If guess is incorrect then add guess to the List
+    guesses.append(guess)
+
+    # when testing the first guess, guesses[-2]==0 evaluates false
+    # and bring us down to the second section section
+
+    if guesses[-2]:
+        if abs(num-guess) < abs(num-guesses[-2]):
+            print('WARMER')
+        else:
+            print('COLDER')
+
+    else:
+        if abs(num-guess) <= 10:
+            print('WARM!')
+        else:
+            print('COLD!')
